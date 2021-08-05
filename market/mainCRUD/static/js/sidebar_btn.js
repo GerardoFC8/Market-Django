ed65c509f33aa840afeb7b9e6f9fca57822c92f3
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-08-05 12:23:53 -0500

    estilos

--- market/mainCRUD/static/js/sidebar_btn.js
+++ market/mainCRUD/static/js/sidebar_btn.js
@@ -1,1 +1,22 @@
-var x = document.getElementsByClassName("example");
+var url = window.location.href;
+var btn_actual = "ninguna";
+
+switch(true) {
+  case (url.indexOf("producto") != -1):
+    btn_actual = document.getElementById("btn-producto");
+    btn_actual.style.backgroundColor = "#b10450";
+    break;
+
+  case (url.indexOf("home") != -1):
+    btn_actual = document.getElementById("btn-home");
+    btn_actual.style.backgroundColor = "#b10450";
+    break;
+
+  case (url.indexOf("usuario") != -1):
+    btn_actual = document.getElementById("btn-usuario");
+    btn_actual.style.backgroundColor = "#b10450";
+    break;
+}
+
+// console.log(btn_actual);
+// console.log(url);
