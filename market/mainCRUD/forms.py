69b99f58f4e351ffd077e0b862011176124b2ae9
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-08-13 17:18:07 -0500

    modo serio

--- market/mainCRUD/forms.py
+++ market/mainCRUD/forms.py
@@ -10,8 +10,8 @@ class ProductoForm(forms.ModelForm):
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirma contraseña', widget=forms.PasswordInput)
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
